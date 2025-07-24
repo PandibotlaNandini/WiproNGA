fd6862fdb35cb6f58bd8858c693a877a0d2040bb
PandibotlaNandini <nandinipandibotla2003@gmail.com>
2025-07-24 21:26:27 +0530

    Day4

---
+++ Day4/IntfTest/IntfTest/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntfTest
+{
+    internal class Test : IOne , ITwo
+    {
+        void IOne.Show()
+        {
+            Console.WriteLine("Show Method from Interface One...");
+        }
+
+        void ITwo.Show()
+        {
+            Console.WriteLine("Show Method from Interface Two...");
+        }
+    }
+}
---
+++ Day4/OopsExample/OopsExample/StaticClassEx1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsExample
+{
+    static class Demos
+    {
+        public static void Show()
+        {
+            Console.WriteLine("Show Method from Demo Class...");
+        }
+
+        public static string Trainer()
+        {
+            return "Trainer is Prasanna P";
+        }
+    }
+    internal class StaticClassEx1
+    {
+        static void Main()
+        {
+            Demos.Show();
+            Console.WriteLine(Demos.Trainer());
+        }
+    }
+}

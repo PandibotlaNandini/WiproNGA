bdaa8d13b874b4d5b2381f15b7beaf969fd7d527
PandibotlaNandini <nandinipandibotla2003@gmail.com>
2025-07-25 21:05:39 +0530

    Day5

---
+++ Day5/FilesExample/FilesExample/BinaryWriterExample3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesExample
+{
+    internal class BinaryWriterExample3
+    {
+        static void Main()
+        {
+            FileStream fs = new FileStream(@"C:\Users\nandi\OneDrive\WiproTraining\Day5\FilesExample\data.txt", FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(12);
+            bw.Write("Wipro");
+            bw.Write(28832.33);
+            bw.Write(true);
+            bw.Close();
+            fs.Close();
+            Console.WriteLine("Primitive Data Stored in files...");
+        }
+    }
+}
---
+++ Day5/FilesExample/FilesExample/FileReadExample1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesExample
+{
+    internal class FileReadExample1
+    {
+        static void Main()
+        {
+            FileStream fs =
+                new FileStream(@"C:\Users\nandi\OneDrive\WiproTraining\Day5\PropertiesExample\PropertiesExample\AutoImplemented.cs", FileMode.Open,
+                FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+            sr.Close();
+            fs.Close();
+        }
+    }
+}

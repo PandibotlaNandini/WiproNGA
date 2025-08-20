ed5fb9c8580c82c625b9f605e8f310039bf40b71
PandibotlaNandini <nandinipandibotla2003@gmail.com>
2025-08-20 22:17:45 +0530

    jwt

---
+++ Aug20/JwtExampleDotnet/JwtExampleDotnet/Controllers/SomeProtectedController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace JwtExampleDotnet.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeProtectedController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProtectedData()
+        {
+            return Ok(new { message = "This is protected data" });
+        }
+    }
+}
